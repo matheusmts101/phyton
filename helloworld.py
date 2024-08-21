a1dce84496ab033b6ca74734854828ef0f929c66
Matheus Serafim <117188864+matheusmts101@users.noreply.github.com>
2024-08-20 22:15:14 -0300

    testes
    teste curso!!

---
+++ helloworld.py
@@ -0,0 +1,98 @@
+print ("Olá, Python!")
+
+print ("Matheus")
+
+#print (Matheus)
+
+print ()
+
+#print "Matheus"
+
+print ('Matheus')
+
+#print ('Matheus') print ('Serafim')
+
+print ('Matheus'); print('Serafim')
+
+print ("matheus","Serafim")
+
+print(10)
+
+print ("teste" ,
+    "teste2")
+
+print ("A aranha pequenininha\nsubiu a tromba d'água.")
+
+#print("\")
+
+print("\"")
+
+print("\\")
+
+print ("1\t2\t3\t4")
+print ("10\t20\t30\t40")
+print ("1234567\t21234567\t31234567\t41234567")
+
+print("A aranha pequenininha" , "subiu" ,5, "vezes", "a tromba d'água.",sep="\n")
+
+#print(sep="\n","A aranha pequenininha" , "subiu" ,5, "vezes", "a tromba d'água.")
+
+
+print ("A menina subiu a tromba d'agua",end="****")
+print ("Veio a chuva e a derrubou")
+
+
+print("Programação","Essenciais","em",sep="***",end='...')
+print("Python")
+
+
+
+print("       *       " * 2)
+print("     *   *     " * 2)
+print("    *     *    " * 2)
+print("   *       *   " * 2)
+print("   ***   ***   " * 2)
+print("     *   *     " * 2)
+print("     *   *     " * 2)
+print("     *****     " * 2)
+
+print("Meu\nnome\né\nBond.", end=" ")
+print("James Bond.")
+ 
+#print(sep="&", "peixe", "salgadinhos")
+
+
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+#print('"Greg's book."')
+ 
+print(0o123)
+ 
+print(0b0010)
+
+print(0b10111)
+
+print(0x17)
+
+print(0o377)
+
+print(4.)
+
+print(.4)
+
+print(3E8)
+
+print(0.0000000000000000000001)
+
+print(True > False)
+print(True < False)
+
+print("\""'Eu sou'"\"", "\""'aprendizado'"\"", "\""'Python'"\"", sep="\n")
+
+print("\"Eu sou\"\n\"\"aprendizado\"\"\n\"\"\"Python\"\"\"")
+
+
+
+
